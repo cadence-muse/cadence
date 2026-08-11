d35202ddc41532146f0f8086dbb66f8ce657db2b
nightnoryu <qazwf734@gmail.com>
2026-08-11 22:50:42 +0300

    Add API routes

--- pkg/cadence/infrastructure/transport/server.go
+++ pkg/cadence/infrastructure/transport/server.go
@@ -29,6 +29,47 @@ func newRESTHandler() publicapi.Handler {
 
 type restHandler struct{}
 
-func (h *restHandler) ListBands(_ context.Context) (publicapi.ListBandsRes, error) {
-	return &publicapi.ListBandsOKApplicationJSON{}, nil
+func (h *restHandler) CreateBand(ctx context.Context, req *publicapi.CreateBandRequestBody) (publicapi.CreateBandRes, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *restHandler) CreateBandTrack(ctx context.Context, req *publicapi.CreateBandTrackRequestBody, params publicapi.CreateBandTrackParams) (publicapi.CreateBandTrackRes, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *restHandler) GetBand(ctx context.Context, params publicapi.GetBandParams) (publicapi.GetBandRes, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *restHandler) GetBandTrack(ctx context.Context, req *publicapi.BandTrack, params publicapi.GetBandTrackParams) (publicapi.GetBandTrackRes, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *restHandler) ListBandTracks(ctx context.Context, params publicapi.ListBandTracksParams) (publicapi.ListBandTracksRes, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *restHandler) ListBands(ctx context.Context) (publicapi.ListBandsRes, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *restHandler) Login(ctx context.Context, req *publicapi.LoginRequestBody) (publicapi.LoginRes, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *restHandler) UpdateBand(ctx context.Context, req *publicapi.UpdateBandRequestBody, params publicapi.UpdateBandParams) (publicapi.UpdateBandRes, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *restHandler) UpdateBandTrack(ctx context.Context, req *publicapi.UpdateBandTrackRequestBody, params publicapi.UpdateBandTrackParams) (publicapi.UpdateBandTrackRes, error) {
+	//TODO implement me
+	panic("implement me")
 }
