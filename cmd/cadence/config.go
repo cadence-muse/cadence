1c5bc7537488ca36a5d2fffa9db8c5ee73e1028f
nightnoryu <qazwf734@gmail.com>
2026-08-11 23:55:19 +0300

    Fix env vars

--- cmd/cadence/config.go
+++ cmd/cadence/config.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/caarlos0/env/v11"
 
@@ -10,23 +11,23 @@ import (
 
 func parseEnv() (*config, error) {
 	c := new(config)
-	if err := env.ParseWithOptions(c, env.Options{Prefix: appID}); err != nil {
+	if err := env.ParseWithOptions(c, env.Options{Prefix: strings.ToUpper(appID) + "_"}); err != nil {
 		return nil, fmt.Errorf("failed to parse env: %w", err)
 	}
 	return c, nil
 }
 
 type config struct {
-	ServeRESTAddress string `env:"serve_rest_address" envDefault:":8080"`
+	ServeRESTAddress string `env:"SERVE_REST_ADDRESS" envDefault:":8080"`
 
-	DBHost     string `env:"db_host"`
-	DBPort     int    `env:"db_port"`
-	DBName     string `env:"db_name"`
-	DBUser     string `env:"db_user"`
-	DBPassword string `env:"db_password"`
+	DBHost     string `env:"DB_HOST"`
+	DBPort     int    `env:"DB_PORT"`
+	DBName     string `env:"DB_NAME"`
+	DBUser     string `env:"DB_USER"`
+	DBPassword string `env:"DB_PASSWORD"`
 
-	DBMaxConn      int `env:"db_max_conn" envDefault:"10"`
-	DBConnLifetime int `env:"db_conn_lifetime" envDefault:"60"`
+	DBMaxConn      int `env:"DB_MAX_CONN" envDefault:"10"`
+	DBConnLifetime int `env:"DB_CONN_LIFETIME" envDefault:"60"`
 }
 
 func (c *config) dsn() postgresql.DSN {
