0b9aa948a10f476256d7c7a0b67b885a0c53388a
nightnoryu <qazwf734@gmail.com>
2026-08-11 17:34:14 +0300

    Add logging middleware for ogen

---
+++ pkg/common/middleware/logging.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/ogen-go/ogen/middleware"
+
+	"cadence/pkg/common/log"
+)
+
+func NewLoggingMiddleware(logger log.Logger) middleware.Middleware {
+	return func(request middleware.Request, next middleware.Next) (middleware.Response, error) {
+		start := time.Now()
+		resp, err := next(request)
+		duration := time.Since(start).String()
+
+		fields := log.Fields{
+			"args":     getParamsForLog(request),
+			"duration": duration,
+			"method":   request.OperationID,
+		}
+
+		loggerWithFields := logger.WithFields(fields)
+		if err == nil {
+			loggerWithFields.Info("call finished")
+		} else {
+			loggerWithFields.Error(err, "call failed")
+		}
+
+		return resp, err
+	}
+}
+
+func getParamsForLog(request middleware.Request) any {
+	var params map[string]any
+	if len(request.Params) > 0 {
+		params = make(map[string]any)
+		for param, value := range request.Params {
+			params[param.Name] = value
+		}
+	}
+	result := make(map[string]any)
+	if len(params) > 0 {
+		result["params"] = params
+	}
+	if request.Body != nil {
+		result["body"] = request.Body
+	}
+	return result
+}
